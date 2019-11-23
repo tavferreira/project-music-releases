7e67b205855dc6ea4cdcae05f67497d405646954
Tiago Ferreira <tiagoferreira@tuta.io>
2019-11-23 13:46:25 +0100

    Requirements met and stretch goal first attempt

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,33 @@
 import React from 'react'
+import { Albums } from './components/Albums.js'
+import { Playlist } from './components/Playlists.js'
 import data from './data.json'
-
-console.log(data)
+import playlistData from './stretch-goal.json'
+import './app.css'
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <main>
+      <aside>
+        <h1>Playlists</h1>
+        <section className="playlists">
+          {playlistData.playlists.items.map((playlist) => (
+            <Playlist
+              key={playlist.id}
+              playlist={playlist} />
+          ))}
+        </section>
+      </aside>
+      <div>
+        <h1>New albums & singles</h1>
+        <section className="albumList">
+          {data.albums.items.map((album) => (
+            <Albums
+              key={album.id}
+              album={album} />
+          ))}
+        </section>
+      </div>
+    </main>
   )
 }
